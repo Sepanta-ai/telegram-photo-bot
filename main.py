914faec1f6b17eb4d3e0d7c1dcc6fabf6bb16c37
Sepanta-ai <Sepanta@zetata.onmicrosoft.com>
2026-02-12 23:28:53 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,39 @@
 import os
+import time
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
 
+# ===== Anti Sleep Web Server =====
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import threading
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Bot is alive")
+
+def run_web():
+    server = HTTPServer(("0.0.0.0", 8080), Handler)
+    server.serve_forever()
+
+threading.Thread(target=run_web).start()
+
+# ===== Telegram Bot =====
 TOKEN = os.getenv("TOKEN")
 GROUP_CHAT_ID = int(os.getenv("GROUP_CHAT_ID"))
 
+# محدودیت اسپم
+user_last_photo = {}
+SPAM_DELAY = 30
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("سلام لطفا عکس مربوطه را ارسال کنید")
+    await update.message.reply_text("سلام 👋\nلطفا عکس همراه کپشن ارسال کنید.")
 
 async def handle_photo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    photo = update.message.photo[-1]
-    await context.bot.send_photo(
-        chat_id=GROUP_CHAT_ID,
-        photo=photo.file_id
-    )
+
+    user_id = update.message.from_user.id
+    now = time.time()
 
 app = ApplicationBuilder().token(TOKEN).build()
 
